1c4ac6ee8194b2e84eb5378040beef3896c8f13a
yanghoonseok <gnstjr5911@gmail.com>
2025-05-18 15:24:14 +0900

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,58 @@
-import React, { useState} from 'react';
+import React, {useEffect, useState} from 'react';
+
+const data = [
+    {
+        "userId": 1,
+        "id": 1,
+        "title": "delectus aut autem",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 2,
+        "title": "quis ut nam facilis et officia qui",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 3,
+        "title": "fugiat veniam minus",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 4,
+        "title": "et porro tempora",
+        "completed": true
+    },
+    {
+        "userId": 1,
+        "id": 5,
+        "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+        "completed": false
+    },
+]
 
 const App = () => {
 
 
     // String
     const [name, setName] = useState("Mr. yang");
-    //number
-    const [age, setAge] = useState(0)
-    // object
-    const [movieData, setMovieData] = useState({
-        "userId": 1,
-        "id": 1,
-        "title": "delectus aut autem",
-        "completed": false
-    })
 
     //array
-    const [movies, setMovies] = useState([
-        {
-            "userId": 1,
-            "id": 1,
-            "title": "delectus aut autem",
-            "completed": false
-        },
-        {
-            "userId": 1,
-            "id": 2,
-            "title": "quis ut nam facilis et officia qui",
-            "completed": false
-        },
-        {
-            "userId": 1,
-            "id": 3,
-            "title": "fugiat veniam minus",
-            "completed": false
-        },
-        {
-            "userId": 1,
-            "id": 4,
-            "title": "et porro tempora",
-            "completed": true
-        },
-        {
-            "userId": 1,
-            "id": 5,
-            "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-            "completed": false
-        },
-    ])
+    const [movies, setMovies] = useState([])
 
+    console.log(1)
     const changeName = ()=> {
         setName("Mr. hoonseok")
-        setAge(30)
+        setMovies(data)
+        console.log(2)
     }
+
+    useEffect(() => {
+        changeName()
+        console.log(3)
+    }, []);
 
     return (
         <div>
@@ -66,7 +66,6 @@ const App = () => {
                 </div>
             ))}
 
-            <h1>{movieData.completed.toString()}</h1>
 
             <button onClick={changeName}>버튼</button>
         </div>
